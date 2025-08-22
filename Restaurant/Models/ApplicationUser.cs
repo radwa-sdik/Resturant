9c63385cdae19eb6b0a155df90932d8cf54713fb
Radwa Mohamed <radwasedeek2013@gmail.com>
2025-08-22 22:14:49 +0300

    Create Models and Database Creation

---
+++ Restaurant/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Restaurant.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Order>? Orders { get; set; }
+    }
+
+}
---
+++ Restaurant/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? UserID { get; set; }
+        public ApplicationUser User { get; set; }
+        public decimal TotalPrice { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+
+
+
+}
